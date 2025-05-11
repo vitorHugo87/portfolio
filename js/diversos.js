e04e02f08db64ba322852ea219caa331ed4efd76
vitorHugo87 <vitorpaduan87@gmail.com>
2025-05-11 15:15:31 -0300

    Jogos Favoritos

---
+++ js/diversos.js
@@ -0,0 +1,18 @@
+// Importa Header e Footer
+fetch('../pages/header.html').then(response => response.text()).then(data => {
+    document.getElementById('header').innerHTML = data;
+
+    // Seleciona todos os <a> dentro da navbar
+    const links = document.querySelectorAll('.navbar-nav .nav-link');
+
+    // Adiciona ativo ao link da pagina atual
+    links.forEach(link => {
+        if(link.innerHTML === 'Diversos') link.classList.add('active');
+    });
+});
+
+fetch('footer.html').then(response => response.text()).then(data => {
+    document.getElementById('footer').innerHTML = data;
+    // Aplica ano atual no copyright do footer
+    document.getElementById('ano').textContent = new Date().getFullYear();
+});
